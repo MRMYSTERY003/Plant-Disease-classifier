fb30d2aa31c129ad711f8909902bbb84e14d1f37
Sriram <80098044+MRMYSTERY003@users.noreply.github.com>
2021-09-18 17:59:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import re
+from flask import Flask, render_template, request, Markup
+from model import predict_image
+import utils
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            file = request.files['file']
+            img = file.read()
+            prediction = predict_image(img)
+            print(prediction)
+            res = Markup(utils.disease_dic[prediction])
+            return render_template('results.html', status=200, result=res)
+        except:
+            pass
+    return render_template('index.html', status=500, res="Internal Server Error")
+
+
+@app.route('/weather', methods = ['GET'])
+def weather():
+    return render_template('new_weather.html')
+
+@app.route('/pre',methods = ['GET'])
+def pre():
+    return render_template('server.html')
+
+@app.route('/about',methods = ['GET'])
+def about():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
