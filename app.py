0431c28e3d5e92a5de0388ac4ed0c30489755e4c
Sriram <80098044+MRMYSTERY003@users.noreply.github.com>
2022-02-22 16:52:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,12 +37,12 @@ def esp():
     try:
 
             print(f'request ===> {request}')
-            file=json.loads(request.data)
-            print(f'file===> {file}')
+            file=request.data
+            print(f'data===> {file}')
             # Read the image via file.stream
-            img = file['image']
-            print(f'image ==> {img}')
-            decoded = b4.b64decode(img)
+            #img = file['image']
+            #print(f'image ==> {img}')
+            decoded = b4.b64decode(file)
             prediction = predict_image(decoded)
             print(prediction)
             res = Markup(api_res.disease_dic[prediction])
